572b6756eadf7a992bcfe6fada7c5d9c37de0e8d
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-08-11 11:37:21 +0300

    Add project files.

---
+++ SortAlgorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sorting = new Sortings(new int[] { 7, 4, 9, 3, 6, 5 });
+
+            //  var result = sorting.BubbleSorting();
+            // var result = sorting.ShakerSorting();
+            var result = sorting.InsertSorting();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ SortAlgorithms/Sortings.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortAlgorithms
+{
+    public class Sortings
+    {
+        private int[] arr;
+        public Sortings(int[] collection)
+        {
+            arr = collection;
+        }
+
+        public IEnumerable<int> BubbleSorting()
+        {
+            if (arr != null)
+            {
+                for (int i = 0; i < arr.Count(); i++)
+                {
+                    for (int j = 0; j < arr.Count() - i - 1; j++)
+                    {
+                        var a = arr[j];
+                        var b = arr[j + 1];
+                        if (a > b)
+                        {
+                            Swap(j, j + 1);
+                        }
+                    }
+                }
+                return arr;
+            }
+            else
+            {
+                return default(int[]);
+            }
+        }
+
+        public IEnumerable<int> ShakerSorting()
+        {
+            if (arr != null)
+            {
+                int left = 0;
+                int right = arr.Length - 1;
+
+                while (left < right)
+                {
+                    for (int i = left; i < right; i++)
+                    {
+                        var a = arr[i];
+                        var b = arr[i + 1];
+                        if (a > b)
+                        {
+                            Swap(i, i + 1);
+                        }
+                    }
+                    right--;
+
+                    for (int i = right; i > left; i--)
+                    {
+                        var a = arr[i];
+                        var b = arr[i - 1];
+                        if (b > a)
+                        {
+                            Swap(i - 1, i);
+                        }
+                    }
+                    left++;
+                }
+                return arr;
+            }
+            else
+            {
+                return default(int[]);
+            }
+        }
+
+        public IEnumerable<int> InsertSorting()
+        {
+            if (arr != null)
+            {
+
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    var temp = arr[i];
+                    var j = i;
+                    while (j > 0 && arr[j - 1] > temp)
+                    {
+                        arr[j] = arr[j - 1];
+                        --j;
+                    }
+                    arr[j] = temp;
+                }
+                return arr;
+            }
+            else
+            {
+                return default(int[]);
+            }
+        }
+
+        private void Swap(int k, int v)
+        {
+            var temp = arr[k];
+            arr[k] = arr[v];
+            arr[v] = temp;
+        }
+    }
+}
